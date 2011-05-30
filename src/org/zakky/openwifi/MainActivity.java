7d90b01a6b4cdca96c22530e9e2c0c7aba4f9e6f
YAMAZAKI Makoto <makoto1975@gmail.com>
2011-05-30 12:59:50 +0900

    20110528 に横浜支部でライブコーディングした際の、変更前のプロジェクト

---
+++ src/org/zakky/openwifi/MainActivity.java
@@ -0,0 +1,24 @@
+
+package org.zakky.openwifi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void onButtonClick(View view) {
+        final Intent intent = new Intent(WifiManager.ACTION_PICK_WIFI_NETWORK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+}
