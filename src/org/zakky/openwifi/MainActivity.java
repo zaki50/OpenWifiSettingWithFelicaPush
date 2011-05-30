ba2909a6ec7f3dca74b16bca710267e448731a66
YAMAZAKI Makoto <makoto1975@gmail.com>
2011-05-30 13:04:42 +0900

    Step2: 送信結果を受け取り、結果をユーザーに通知する

--- src/org/zakky/openwifi/MainActivity.java
+++ src/org/zakky/openwifi/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import jp.andeb.kushikatsu.helper.KushikatsuHelper;
 
 public class MainActivity extends Activity {
+    private static final int KUSHIKATUS_REQUEST_CODE = 1;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,21 @@ public class MainActivity extends Activity {
     public void onButtonClick(View view) {
         final Intent intent = new Intent(WifiManager.ACTION_PICK_WIFI_NETWORK);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(KushikatsuHelper.buildIntentForSendIntent(intent));
+        startActivityForResult(KushikatsuHelper.buildIntentForSendIntent(intent),
+                KUSHIKATUS_REQUEST_CODE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != KUSHIKATUS_REQUEST_CODE) {
+            return;
+        }
+        if (resultCode == Activity.RESULT_OK
+                || resultCode == KushikatsuHelper.RESULT_PUSH_REGISTERED) {
+            Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "NG", Toast.LENGTH_LONG).show();
+        }
+    }
 }
